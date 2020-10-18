8d844e5c7702733f6e4e11a10f4664aafeb9125e
Kesavan2006 <69773905+Kesavan2006@users.noreply.github.com>
2020-10-18 12:05:05 -0700

    Update AutonomousNavi.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousNavi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousNavi.java
@@ -32,8 +32,19 @@ public class AutonomousNavi extends LinearOpMode {
         {
 
                                  //Place Code Here
-
-
+    MecanumDirectionalFunction m = new MecanumDirectionalFunction();             //Instantiate object m of class MecanumDirectionalFunction *Object does not have to be called m*
+    m.Calculation(0, 1, 1);                  //Calculate
+    FrontLeftDrive.setPower(m.GetFrontLeftPower(1));                              //Set Motor powers
+    BackLeftDrive.setPower(m.GetBackLeftPower(1));
+    FrontRightDrive.setPower(m.GetFrontRightPower(1));
+    BackRightDrive.setPower(m.GetBackRightPower(1));
+    idle(1s)
+        MecanumDirectionalFunction d = new MecanumDirectionalFunction();             //Instantiate object m of class MecanumDirectionalFunction *Object does not have to be called m*
+    d.Calculation(4, 0, 1);                  //Calculate
+    FrontLeftDrive.setPower(m.GetFrontLeftPower(1));                              //Set Motor powers
+    BackLeftDrive.setPower(m.GetBackLeftPower(0));
+    FrontRightDrive.setPower(m.GetFrontRightPower(0));
+    BackRightDrive.setPower(m.GetBackRightPower(1));
 
 
 
