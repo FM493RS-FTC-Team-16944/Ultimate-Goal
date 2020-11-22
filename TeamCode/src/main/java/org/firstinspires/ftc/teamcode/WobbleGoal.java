f100326ce7aaddefac226285da4c3ad265b3d472
hari <harinsrikanth@gmail.com>
2020-11-21 18:40:27 -0800

    tf testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleGoal.java
@@ -147,36 +147,39 @@ public class WobbleGoal extends LinearOpMode {
 
          */
 
-        //Tensor flow start:
-        if (tfod != null) {
-            // getUpdatedRecognitions() will return null if no new information is available since
-            // the last time that call was made.
-            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-            if (updatedRecognitions != null) {
-                telemetry.addData("# Object Detected", updatedRecognitions.size());
+        int j;
+        for (j=0;j<10;j++) {
+            //Tensor flow start:
+            if (tfod != null) {
+                // getUpdatedRecognitions() will return null if no new information is available since
+                // the last time that call was made.
+                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                if (updatedRecognitions != null) {
+                    telemetry.addData("# Object Detected", updatedRecognitions.size());
 
-                // step through the list of recognitions and display boundary info.
-                int i = 0;
-                for (Recognition recognition : updatedRecognitions) {
-                    telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                    telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                            recognition.getLeft(), recognition.getTop());
-                    telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                            recognition.getRight(), recognition.getBottom());
-                }
-                telemetry.update();
-                if (updatedRecognitions.size()==1){
-                    Motion = 1;
-                } else if (i==2){
-                    Motion =2;
-                }else if (updatedRecognitions.size()==3){
-                    Motion =3;
-                }else if (updatedRecognitions.size()==4){
-                    Motion =4;
-                }else if (updatedRecognitions.size()==0){
-                    Motion =0;
-                }
+                    // step through the list of recognitions and display boundary info.
+                    int i = 0;
+                    for (Recognition recognition : updatedRecognitions) {
+                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                recognition.getLeft(), recognition.getTop());
+                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                recognition.getRight(), recognition.getBottom());
+                    }
+                    telemetry.update();
+                    if (updatedRecognitions.size() == 1) {
+                        Motion = 1;
+                    } else if (i == 2) {
+                        Motion = 2;
+                    } else if (updatedRecognitions.size() == 3) {
+                        Motion = 3;
+                    } else if (updatedRecognitions.size() == 4) {
+                        Motion = 4;
+                    } else if (updatedRecognitions.size() == 0) {
+                        Motion = 0;
+                    }
 
+                }
             }
         }
 
